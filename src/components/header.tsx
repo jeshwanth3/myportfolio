c99afc75e857a5aa25f01e5342eae728566b6817
Jeshwanth Goud <jeshwanthgoud3@gmail.com>
2025-05-04 23:49:39 +0000

    k

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -76,8 +76,7 @@ export function Header() {
                 <VisuallyHidden>Toggle Menu</VisuallyHidden>
               </Button>
             </SheetTrigger>
-            <SheetContent title="Mobile Navigation"
-              side="right"
+            <SheetContent side="right" title="Mobile Navigation"
               className="w-[85vw] max-w-[380px] bg-background/95 border-l border-border/30 backdrop-blur-xl p-0 flex flex-col"
             >
               <VisuallyHidden>
