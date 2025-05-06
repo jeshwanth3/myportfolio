231dc2348e3a65cceee9e5167533814a88c0014f
Jeshwanth Goud <jeshwanthgoud3@gmail.com>
2025-05-06 06:28:37 +0000

    hghhhhhhhhhhhhhhh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import BasicButton from '@/components/basic-button';
-import React, { useState } from "react";
 import { Header } from "@/components/header";
-import { SummarySection } from "@/components/summary-section";
 import { ExperienceSection } from "@/components/experience-section";
 import { SkillsSection } from "@/components/skills-section";
 import { ProjectsSection } from "@/components/projects-section"; // Import ProjectsSection
@@ -9,24 +6,21 @@ import { EducationSection } from "@/components/education-section";
 import { ContactSection } from "@/components/contact-section";
 import { Footer } from "@/components/footer";
 
+import { SummarySection } from "@/components/summary-section";
 
-export default function Home() {    
-  const [open, setOpen] = useState<boolean>(false);
+export default function Home() {
   return (
     // The main container now spans the full viewport height
     <div className="flex flex-col min-h-screen bg-background">
 
       {/* Content is rendered directly */}
-      <Header open={open} setOpen={setOpen} />
-      
+      <Header />
+
       <main className="flex-1 px-4 md:px-6">
         <SummarySection />
         <ExperienceSection />
         <ProjectsSection />
         <SkillsSection />
-        <div className='p-4'>
-            <BasicButton>Click Me</BasicButton>
-          </div>
         <EducationSection />
         <ContactSection />
       </main>
