f2625152922525e91a30db1984096e909ea81f49
Jeshwanth Goud <69040423+jeshwanth3@users.noreply.github.com>
2025-06-08 21:09:47 +0000

    z

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,7 +74,7 @@ export default function RootLayout({
         suppressHydrationWarning
       >
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-          <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="w-full px-4 sm:px-6 lg:px-8">
             {children}
           </div>
           <Toaster />
